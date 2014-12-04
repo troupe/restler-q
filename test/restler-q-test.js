3f2a03b1c0dd9b0b7ca68b3d9720d19d60eaf15a
Andrew Newdigate <andrew@troupe.co>
2014-12-04 11:56:31 +0000

    Allow spread with the response

--- test/restler-q-test.js
+++ test/restler-q-test.js
@@ -35,3 +35,41 @@ describe('restler-q', function(){
 
   });
 });
+
+describe('restler-q-spread', function(){
+  describe('#get()', function() {
+    it('should resolve correctly', function(done) {
+      rest.spread.get('https://api.github.com/orgs/Troupe/repos')
+        .spread(function(repos, response) {
+          assert(Array.isArray(repos));
+          assert(response);
+        })
+        .nodeify(done);
+    });
+
+    it('should fail correctly', function(done) {
+      rest.spread.get('https://api.github.com/orgs/Troupe123897123897123879132789123DOES_NOT_EXIST/repos')
+        .spread(function() {
+          assert(false, 'Expected a failure');
+        })
+        .fail(function(err) {
+          assert(err);
+          assert(err.response);
+        })
+        .nodeify(done);
+    });
+
+    it('should handle errors correctly', function(done) {
+      rest.spread.get('https://hostname_does_not_exist.11298371/')
+        .spread(function() {
+          assert(false, 'Expected an error');
+        })
+        .fail(function(err) {
+          assert(err instanceof Error);
+        })
+        .nodeify(done);
+    });
+
+  });
+});
+
